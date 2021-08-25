7b6ed9a3417d46a89aabbc2f97069ac90e251e29
Daniel <narselldev@gmail.com>
2021-08-25 11:39:51 -0500

    Server setup and env variables updated

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log("eeee")
+const { ApolloServer } = require('apollo-server');
+
+const typeDefs = require('./typeDefs');
+const resolvers = require('./resolvers');
+const PaymentsAPI = require('./dataSources/paymentsApi');
+
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    dataSources: () => ({
+        paymentsAPI: new PaymentsAPI()
+    }),
+    introspection: true,
+    playground: true
+
+});
+
+server.listen(process.env.PORT || 4000).then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`);
+});
--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 module.exports = {
-    payments_api_url: process.env.USERS_API_URL || 'https://mtic-pagos-ms.herokuapp.com',
-    inventory_api_url: process.env.ACCOUNT_API_URL || 'https://inventory-appweb.herokuapp.com',
+    payments_api_url: process.env.PAYMENTS_API_URL || 'https://mtic-pagos-ms.herokuapp.com',
+    inventory_api_url: process.env.INVENTORY_API_URL || 'https://inventory-appweb.herokuapp.com',
     
 };
